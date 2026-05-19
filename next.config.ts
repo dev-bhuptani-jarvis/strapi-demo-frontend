8530d5492922b13a489c9381b26ba3f0e4e6b65b
dev.bhuptani <devbhuptani.jarvis@gmail.com>
2026-05-19 13:30:37 +0530

    Fix Vercel routing

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  reactStrictMode: false,
-  distDir: "build",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "127.0.0.1",
-        port: "1337",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
-  turbopack: {
-    root: __dirname,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
